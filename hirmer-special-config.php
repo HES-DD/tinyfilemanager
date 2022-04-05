240e421748f667a1402447b9f77bced47e2df661
fjw <fjw@garx.de>
2022-04-05 10:42:47 +0200

    Logo + bestimmte Ordner sperren + eigenes Config-File

---
+++ hirmer-special-config.php
@@ -0,0 +1,20 @@
+<?php
+
+const HRM_CONFIG = [ // für den CDN-Test-Link
+    "cdn_domain" => "http://n.hirmercdn.de",
+
+    "content_paths" => [ // Dateien innerhalb dieser Ordner haben den Copy/Paste-Link zum CDN
+        FM_ROOT_PATH . "/hgg/content",
+        FM_ROOT_PATH . "/hrm/content",
+        FM_ROOT_PATH . "/eck/content"
+    ],
+
+    "folders_forbidden_to_modify" => [ // Bearbeitungssymbole sind für diese ausgeblendet (ausgeblendet! nicht secure!)
+        "/hgg",
+        "/hrm",
+        "/eck",
+        "/hgg/content",
+        "/hgg/products",
+
+    ]
+];
